2208c7cf7ac62a9e521b9df87b3af1124b02532b
gargmahak93 <gargmahak93@gmail.com>
2015-01-02 18:55:13 +0530

    Angular JS

---
+++ myUsers.js
@@ -0,0 +1,51 @@
+function userController($scope) {
+$scope.fName = '';
+$scope.lName = '';
+$scope.passw1 = '';
+$scope.passw2 = '';
+$scope.users = [
+{id:1, fName:'Mahak',  lName:"Garg" },
+{id:2, fName:'abc',   lName:"abc" },
+{id:3, fName:'xyz',   lName:"xyz" },
+{id:4, fName:'pqr',  lName:"pqr" },
+{id:5, fName:'Raj',  lName:"Simran" },
+{id:6, fName:'Peter', lName:"sandic" }
+];
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false; 
+
+$scope.editUser = function(id) {
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    $scope.fName = '';
+    $scope.lName = '';
+    } else {
+    $scope.edit = false;
+    $scope.fName = $scope.users[id-1].fName;
+    $scope.lName = $scope.users[id-1].lName; 
+  }
+};
+
+$scope.$watch('passw1',function() {$scope.test();});
+$scope.$watch('passw2',function() {$scope.test();});
+$scope.$watch('fName', function() {$scope.test();});
+$scope.$watch('lName', function() {$scope.test();});
+
+$scope.test = function() {
+  if ($scope.passw1 !== $scope.passw2) {
+    $scope.error = true;
+    } else {
+    $scope.error = false;
+  }
+  $scope.incomplete = false;
+  if ($scope.edit && (!$scope.fName.length ||
+  !$scope.lName.length ||
+  !$scope.passw1.length || !$scope.passw2.length)) {
+       $scope.incomplete = true;
+  }
+};
+
+}
+
